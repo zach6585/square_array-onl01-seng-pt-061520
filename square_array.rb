dbe970e8b5e9c57741cb179ab6c2b05aac594632
zachary mines <zmines@wesleyan.edu>
2020-06-19 16:25:21 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
 def square_array(array)
-  # your code here
+  new_array = []
+  array.each do |num|
+    new_array.push(num*num)
+  end
+  return new_array
+end
+
+def adv_square_array(array)
+  array.collect{|num| num*num}
 end
